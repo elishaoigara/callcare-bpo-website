0cebe1dd9a56daedfa9fe56151ec9995258dc71f
elishaoigara <1.72077973e+08+elishaoigara@users.noreply.github.com>
2026-08-19 18:28:03 +0000

    Use exact arrow logo in site and favicon

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -26,8 +26,8 @@ const operationsImage = "/manus-storage/callcare-operations_3f2206be.png";
 const servicesImage = "/manus-storage/callcare-services_bb91f26c.png";
 const teamImage = "/manus-storage/callcare-team_dc14a11a.png";
 const logoPrimary = "/brand/logo_primary_horizontal.svg";
-const logoIconCream = "/brand/icon_favicon.svg";
-const logoIconDark = "/brand/icon_mono_dark.svg";
+const logoIconCream = "/brand/callcare-symbol-exact.svg";
+const logoIconDark = "/brand/callcare-symbol-exact.svg";
 
 const services = [
   {
